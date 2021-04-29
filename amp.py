c270f6ac9ac5e0a1cd67db08bb239fcbee2bbd36
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2021-04-29 17:08:26 +0800

    Update amp.py

--- amp.py
+++ amp.py
@@ -50,7 +50,7 @@ class AMP(Optimizer):
                 for p in group['params']:
                     if p.grad is not None:
                         if i == 0:
-                            self.state[p]['dev'] = 0
+                            self.state[p]['dev'] = torch.zeros_like(p.grad)
                         dev = self.state[p]['dev'] + group['inner_lr'] * p.grad
                         clip_coef = group['epsilon'] / (dev.norm() + 1e-12)
                         dev = clip_coef * dev if clip_coef < 1 else dev
